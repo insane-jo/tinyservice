a08aade824c8bdd7040da099f923c9693e5b1ff0
Aleksei Fedorov <hitechrussia@gmail.com>
2017-05-17 01:45:23 +0300

    subscriptions handlers tests

--- tests/client.spec/02.subscription.test.js
+++ tests/client.spec/02.subscription.test.js
@@ -1,12 +1,7 @@
 const TinyService = require('../../index');
-const DUMMY_PORT = (Math.random() * (65535 - 10000) + 10000)|0;
+const DUMMY_PORT = (Math.random() * (65535 - 10000) + 10000) | 0;
 
 let server = new TinyService.Server({
-    port: DUMMY_PORT
-});
-
-let client = new TinyService.Client({
-    host: '127.0.0.1',
     port: DUMMY_PORT
 });
 
@@ -26,7 +21,13 @@ test('Subscriptions emits multiple times, unless called unsubscribe', (done) => 
         setTimeout(() => done(null, {}), 2);
         setTimeout(() => done(null, {}), 3);
 
-        registerUnsubscribe(() => {});
+        registerUnsubscribe(() => {
+        });
+    });
+
+    let client = new TinyService.Client({
+        host: '127.0.0.1',
+        port: DUMMY_PORT
     });
 
     let totalCalledCount = 0;
@@ -38,11 +39,53 @@ test('Subscriptions emits multiple times, unless called unsubscribe', (done) => 
     setTimeout(() => {
         expect(totalCalledCount).toBe(3);
         done();
+        client.close();
     }, 500);
 });
 
 test('On unsubscribe - server removes handler for this subscription', (done) => {
+    (new Promise(function (resolve) {
+        let totalCalledCount = 0;
 
+        server.add({
+            a: 1
+        }, (msg, done, registerUnsubscribe) => {
+            setTimeout(() => done(null, {}), 100);
+            setTimeout(() => done(null, {}), 200);
+            setTimeout(() => {
+                done(null, {});
+                setTimeout(() => {
+                    resolve(totalCalledCount)
+                }, 100);
+            }, 300);
+
+            registerUnsubscribe(() => {
+            });
+        });
+
+        let client = new TinyService.Client({
+            host: '127.0.0.1',
+            port: DUMMY_PORT
+        });
+
+        client.subscribe({a: 1}, () => {
+            totalCalledCount++;
+            if (totalCalledCount === 2) {
+                client.unsubscribe({a: 1});
+            }
+        });
+
+        // setTimeout(() => {
+        //     expect(totalCalledCount).toBe(2);
+        //     done();
+        //     client.close();
+        // }, 500);
+    })).then(function (totalCalledCount) {
+        expect(totalCalledCount).toBe(2);
+        done();
+    }).catch((e) => {
+        debugger
+    });
 });
 
 test('When calling clear on server instance - all handlers and subscriptions removes', (done) => {
